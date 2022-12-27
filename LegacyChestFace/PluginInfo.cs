a798ef003df7caa1f4124824351f360af5c56266
dev9998 <81720436+developer9998@users.noreply.github.com>
2022-12-26 23:15:21 -0330

    Bump

--- LegacyChestFace/PluginInfo.cs
+++ LegacyChestFace/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "com.dev9998.gorillatag.legacychestface";
         public const string Name = "LegacyChestFace";
-        public const string Version = "1.1.0";
+        public const string Version = "1.1.1";
     }
 }
